b1e90c8fc254d5eca0d40114aff1dc7a80e21664
Khurmatillo <72973599+MRXDeveloper23@users.noreply.github.com>
2022-07-25 16:12:58 +0500

    angular project initialized with ant design ui

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'signup', loadChildren: () => import('./auth/signup/signup.module').then(m => m.SignupModule) }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/pages/welcome/welcome.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { IconsProviderModule } from 'src/app/icons-provider.module';
+
+import { WelcomeRoutingModule } from './welcome-routing.module';
+
+import { WelcomeComponent } from './welcome.component';
+
+@NgModule({
+  imports: [
+    WelcomeRoutingModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+  ],
+  declarations: [WelcomeComponent],
+  exports: [WelcomeComponent],
+})
+export class WelcomeModule {}
